b7c950e02126462a63ef30435376df9cc55aa930
sayadav96 <sayadav96@gmail.com>
2025-09-22 17:56:27 +0530

    changed the app password and replaced test with prod password

--- app/api/referral/route.js
+++ app/api/referral/route.js
@@ -5,32 +5,35 @@ import path from "path";
 export async function POST(req) {
   try {
     const body = await req.json();
-    const { yourName, yourNumber, yourEmail, studyDestination, intakeYear } =
-      body;
+    const {
+      yourName,
+      yourEmail,
+      yourNumber,
+      referralName,
+      referralEmail,
+      referralNumber,
+    } = body;
 
-    // Read HTML template
     const templatePath = path.join(
       process.cwd(),
       "mail",
       "templates",
-      "contactTemplate.html"
+      "referalTemplate.html"
     );
-    let htmlTemplate = fs.readFileSync(templatePath, "utf8");
 
-    // Pick the correct base URL
+    let htmlTemplate = fs.readFileSync(templatePath, "utf8");
     const baseUrl =
       process.env.NEXT_PUBLIC_BASE_URL || "https://vadvisor-nu.vercel.app";
 
-    // Replace placeholders
     htmlTemplate = htmlTemplate
       .replace(/{{ yourName }}/g, yourName)
       .replace(/{{ yourNumber }}/g, yourNumber)
       .replace(/{{ yourEmail }}/g, yourEmail)
-      .replace(/{{ studyDestination }}/g, studyDestination)
-      .replace(/{{ intakeYear }}/g, intakeYear)
+      .replace(/{{ referralName }}/g, referralName)
+      .replace(/{{ referralEmail }}/g, referralEmail)
+      .replace(/{{ referralNumber }}/g, referralNumber)
       .replace(/{{ logoUrl }}/g, `${baseUrl}/images/logo.png`);
 
-    // Configure transporter
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
@@ -39,19 +42,17 @@ export async function POST(req) {
       },
     });
 
-    // Mail options
     const mailOptions = {
       from: `"VAdvisor Overseas" <${process.env.MAIL_USER}>`,
       to: process.env.MAIL_RECEIVER || process.env.MAIL_USER,
-      subject: "New Contact Us Submission",
+      subject: "New Referral Submission received",
       html: htmlTemplate,
     };
-
     await transporter.sendMail(mailOptions);
 
     return new Response(JSON.stringify({ success: true }), { status: 200 });
   } catch (err) {
-    console.error("Email error for contact us:", err);
+    console.error("Email error for Referrral:", err);
     return new Response(
       JSON.stringify({ success: false, error: err.message }),
       { status: 500 }
